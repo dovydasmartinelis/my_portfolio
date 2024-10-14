40265ef9f2f8ecef0cacde13c191f25f30c237d8
dovydasmartinelis <160829192+dovydasmartinelis@users.noreply.github.com>
2024-10-14 11:09:54 +0300

    did some updates

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,27 +1,31 @@
 import styles from './ProjectsStyles.module.css';
 import viberr from '../../assets/viberr.png';
 import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
+import lemon from '../../assets/lemon.png'
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
+
         <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-        h3="Working on it"
-          p="..."
+         src={freshBurger}
+          link="https://todoapp9000.netlify.app/"
+          h3="Todo App"
+          p="An app for your todos"
         />
+
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-      h3="To be announced"
-          p="..."
+         src={lemon}
+          link="https://tenor.com/view/im-working-on-it-progress-trying-my-best-keanu-reeves-john-wick-gif-13730626"
+          h3="Little Lemon Restaurant"
+          p="coming soon"
         />
+
+
+
 
       </div>
     </section>
